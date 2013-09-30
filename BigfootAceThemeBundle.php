dd595ac7cf1898c43c47ad47983d67b19f531e64
Guillaume Manen <g.manen@c2is.fr>
2013-09-30 11:10:30 +0200

    bundle first skeleton

---
+++ BigfootAceThemeBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Bigfoot\Theme\AceThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class BigfootAceThemeBundle
+ * @package Bigfoot\Theme\BigfootAceThemeBundle
+ */
+class BigfootAceThemeBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot() {}
+}
